c735837590b6163ebfd9d7d14ca4e97eb7fe429b
Praveen Subramanian <Praveentechnic@gmail.com>
2021-06-02 20:30:35 +0530

    Added preprocessing

--- aut.py
+++ aut.py
@@ -1,6 +1,7 @@
 
-
-from PIL import ImageGrab
+import cv2
+import os
+from PIL import ImageGrab,Image
 import win32gui
 
 
@@ -14,6 +15,13 @@ toplist, winlist = [], []
 def enum_cb(hwnd, results):
     winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
 
+
+def preprocessImage(imgLoc, saveAs):
+    image = cv2.imread(imgLoc)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold( gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    filename = "{}.png".format(saveAs)
+	cv2.imwrite(filename, gray)
 
 class AUT():
     def __init__(self, windowName) -> None:
@@ -34,7 +42,8 @@ class AUT():
         self.boundingRectangle = bbox
         img = ImageGrab.grab(bbox)
         img.save('aut_img_'+str(hwnd)+'.png')
-        self.imagePath= 'aut_img_'+str(hwnd)+'.png'
+        preprocessImage('aut_img_'+str(hwnd)+'.png', 'aut_img_'+str(hwnd)+'PROCESSED.png')
+        self.imagePath= 'aut_img_'+str(hwnd)+'PROCESSED.png'
 
     def _getBoundingRectangleofProcess(self):
         pass
