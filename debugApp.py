16799c48c6e8e04b232a8c95c27cc3ed37ffce47
Praveen Subramanian <Praveentechnic@gmail.com>
2021-06-05 17:39:39 +0530

    added the missing instance fix

--- debugApp.py
+++ debugApp.py
@@ -6,7 +6,7 @@ import pyautogui
 application = 'Cougar Diagnostics'
 
 IP = imageProcessing.ImageProcessing(windowName=application)
-co = IP.findElement('MachineLayers')
+co = IP.findElement('MachineLayers', 1)
 print(str(co))
 
 pyautogui.click(co[0], co[1])
@@ -14,7 +14,7 @@ pyautogui.click(co[0], co[1])
 pyautogui.doubleClick(co[0], co[1])
 
 IP = imageProcessing.ImageProcessing(windowName=application)
-co = IP.findElement('Channels')
+co = IP.findElement('Channels', 1)
 
 
 pyautogui.doubleClick(co[0], co[1])
