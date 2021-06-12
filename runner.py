8122dde6f774b28b1a304289311a920764ba17de
Praveen Subramanian <Praveentechnic@gmail.com>
2021-06-12 21:46:04 +0530

    Added Assert functionality

--- runner.py
+++ runner.py
@@ -38,15 +38,30 @@ for items in instructionSet:
     print("Running item : "+ str(items))
     isRegx = "Regx" in items.args
     IP = imageProcessing.ImageProcessing(windowName=items.applicationName)
-    coOrdinates = IP.findElement(items.elementName , items.instance , isRegex=isRegx)
 
     if(items.actionName == 'double_click'):
+        coOrdinates = IP.findElement(items.elementName , items.instance , isRegex=isRegx)
         pyautogui.doubleClick(coOrdinates[0],coOrdinates[1])
     elif(items.actionName == 'click' ):
+        coOrdinates = IP.findElement(items.elementName , items.instance , isRegex=isRegx)
         pyautogui.click(coOrdinates[0],coOrdinates[1])
     elif(items.actionName == 'close_application'):
+        coOrdinates = IP.findElement(items.elementName , items.instance , isRegex=isRegx)
         pyautogui.click(coOrdinates[0], coOrdinates[1])
         time.sleep(0.3)
         pyautogui.click(coOrdinates[0], coOrdinates[1])
+    elif(items.actionName == 'assertExits'):
+        coOrdinates = IP.findElement(items.elementName , items.instance , isRegex=isRegx)
+    elif(items.actionName == 'assertNotExits'):
+        found = False
+        try:
+            coOrdinates = IP.findElement(items.elementName , items.instance , isRegex=isRegx)    
+            found = True
+        except:
+            pass
+        if(found):
+            raise Exception("Assertion Failed. Control unexpected , but exists")
+            
+
 
     time.sleep(input_file.config.stepInterval)
